6364f6e0463566779bc34b90a60e9f99a7c01fdd
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-17 08:56:07 +0100

    Added click event listener on button

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Dad Jokes Starter');
+const url = "https://icanhazdadjoke.com/api";
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  console.log("hello");
+});
