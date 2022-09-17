34a863740343e52a5a15f625696bdd257359f467
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-17 09:02:49 +0100

    Added fetch function and minor fix to URL

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const url = "https://icanhazdadjoke.com/api";
+const url = "https://icanhazdadjoke.com/";
 
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
 btn.addEventListener("click", () => {
-  console.log("hello");
+  fetchDadJoke();
 });
+
+const fetchDadJoke = async () => {
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "learning app",
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+};
